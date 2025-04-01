3afed7247fe82d7f88c433caab50f7dbb3205af6
mdemiiir1 <demirmuhammed973@gmail.com>
2025-04-01 17:31:18 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('surpriseButton').addEventListener('click', function() {
+    document.getElementById('container').style.display = 'none';  // İlk sayfayı gizle
+    document.getElementById('surprisePage').style.display = 'block';  // Sürpriz sayfasını göster
+});
